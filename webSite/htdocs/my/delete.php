1cd2a2aa844120be59dba06aa53f076dfa9a8dbf
reivaxy <xavier@c-est-simple.com>
2018-08-19 22:36:31 +0200

    Can now delete a module from the registration table. Added a few icons to the icon font

---
+++ webSite/htdocs/my/delete.php
@@ -0,0 +1,22 @@
+<?
+
+require('../../includes/utils.inc.php');
+
+// mac of the module to remove from modules table
+$mac = $_REQUEST['mac'];
+if(!$mac) {
+  returnError("Missing 'mac' parameter.");
+}
+
+$mysqli = connect();
+$stmt =  $mysqli->stmt_init();
+
+// First check that api_key is valid by fetching the user_id that we'll need to insert
+$stmt->prepare('delete from xiot_my where mac = ?') OR returnError("Invalid delete statement");
+$stmt->bind_param("s", $mac);
+$stmt->execute();
+$stmt->close();
+$mysqli->commit();
+$mysqli->close();
+
+?>
--- webSite/htdocs/my/index.php
+++ webSite/htdocs/my/index.php
@@ -16,44 +16,60 @@ if(count($modules) == 1) {
 ?>
 <!--  
       It's very unlikely that anyone will have 2 iotinator masters 
-      So I'm not spending too much time on it :)
+      So I'm not spending too much time on this output :)
 -->
 <html>
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1"> 
     <link rel="stylesheet" href="css/my.css" />
-  </head>
-    <title>My Iotinator</title>
-    <body>
-        <?
-        $count = count($modules);
-        if($count == 0) {
-          echo "<span class=\"error\">No module found for ip $gatewayIp</span><br/>";          
-          echo '<span>Make sure you are connected to your local network.</span>';          
-        } else {
-?>
-      <table>
-        <tr>
-          <th>Module</th>
-          <th>Local IP</th>
-          <th>Registration date</th>
-        </tr>
-<?        
-          for($i = 0; $i < $count; $i++) {
-            $name = $modules[$i]["name"];
-            $mac = $modules[$i]["mac"];
-            $localIp = htmlspecialchars($modules[$i]["localIp"]);
-            $date = $modules[$i]["date"];
-            
-            echo "<tr>";
-            echo "<td>" . htmlspecialchars($name) . "</td>";
-            echo "<td><a href=\"http://$localIp\">$localIp</a></td>";
-            echo "<td>" . htmlspecialchars($date) . "</td>";
-            echo "</tr>";
+    <script>
+      function deleteModule(name, mac) {
+        if(confirm(`Confirm deletion of module '${name}' ?`)) {
+          const req = new XMLHttpRequest();
+          req.open('GET', '/my/delete.php?mac=' + mac, false); 
+          req.send(null);
+          var id = `_${mac}`;
+          var tr = document.getElementById(id);
+          if(tr) {
+            tr.parentNode.removeChild(tr);
           }
-          echo "</table>";
         }
-        ?>        
-        
-    </body>
+      }
+    </script>
+  </head>
+  <title>My Iotinator</title>
+  <body>
+<?
+    $count = count($modules);
+    if($count == 0) {
+      echo "<span class=\"error\">No module found for ip $gatewayIp</span><br/>";          
+      echo '<span>Make sure you are connected to your local network.</span>';          
+    } else {
+?>
+    <table>
+      <tr>
+        <th>Module</th>
+        <th>Local IP</th>
+        <th>Registration date</th>
+      </tr>
+<?        
+        for($i = 0; $i < $count; $i++) {
+          $name = $modules[$i]["name"];
+          $mac = $modules[$i]["mac"];
+          $localIp = htmlspecialchars($modules[$i]["localIp"]);
+          $date = $modules[$i]["date"];
+          
+          echo "<tr id=\"_". htmlspecialchars($mac) . "\">";
+          echo "<td class=\"name\">" . htmlspecialchars($name) . "</td>";
+          echo "<td class=\"link\"><a href=\"http://$localIp\">$localIp</a></td>";
+          echo "<td class=\"date\">" . htmlspecialchars($date) . "</td>";
+          echo "<td class=\"bin\"><span title=\"Delete\" onClick=\"deleteModule('" . 
+                  htmlspecialchars($name) . "','" . htmlspecialchars($mac) . "')\" class=\"icon icon-bin\"/></td>";
+          echo "</tr>";
+        }
+        echo "</table>";
+      }
+      ?>        
+      
+  </body>
 </html>
